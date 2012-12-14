cbec73a95096238ef62bcfe53b21a07a24bc661c
Jonas Green <jg@schantz.com>
2012-12-14 19:28:52 +0100

    asdf

--- src/com/rankytank/client/App.java
+++ src/com/rankytank/client/App.java
@@ -1,11 +1,17 @@
 package com.rankytank.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
 
 public class App implements EntryPoint {
 
     public void onModuleLoad() {
+        FlowPanel p = new FlowPanel();
+        p.add(new Button("wuuhuhuhuh"));
+        RootLayoutPanel.get().add(p);
     }
 
 
